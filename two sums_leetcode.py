609922b23c702a15deddd9c2ca5f3a420364cfda
Prince <meprincesr@gmail.com>
2022-12-17 17:33:46 +0530

    new

--- two sums_leetcode.py
+++ two sums_leetcode.py
@@ -1,11 +1,11 @@
 num=[]
-for x in range(4):
+for x in range(3):
     x=int(input("Enter the list elements: "))
     num.append(x)
 print(num)
 n=int(input("Check the no. you want to check: "))
 for i in range(len(num)):
-    for j in range(i+1,len(num)-1):
+    for j in range (i+1,len(num)):
         if num[i]+num[j]==n:
             print('The indexes are - ',i,j)
             break
